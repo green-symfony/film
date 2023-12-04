5b1c91add705efd63b8b0f99bb4afac3f3a5ac5a
Grin <son5-29@mail.ru>
2023-12-04 22:27:46 +1200

    fix(update.sh)

--- update.sh
+++ update.sh
@@ -1,10 +1,19 @@
 cd "./bundles/green-symfony"
 cd "./command-bundle"
-git pull
+git fetch --all
+git checkout -b v1 -f -q
+git checkout v1 -f
+git merge origin/v1 --ff
 cd ".."
 cd "./service-bundle"
-git pull
+git fetch --all
+git checkout -b v1 -f -q
+git checkout v1 -f
+git merge origin/v1 --ff
 cd ".."
 cd "./env-processor-bundle"
-git pull
+git fetch --all
+git checkout -b v1 -f -q
+git checkout v1 -f
+git merge origin/v1 --ff
 cd "../../.."

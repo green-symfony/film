b702de838c3606db944441b4e132f5d7dc7cb5d8
Grin <son5-29@mail.ru>
2023-12-15 07:33:34 +1200

    fix(init.sh)

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 mkdir "./bundles/green-symfony" -p
 cd "./bundles/green-symfony"
 git clone "https://github.com/green-symfony/command-bundle.git"
+git checkout -b v1
+git pull origin v1
 git clone "https://github.com/green-symfony/service-bundle.git"
+git checkout -b v1
+git pull origin v1
 git clone "https://github.com/green-symfony/env-processor-bundle.git"
+git checkout -b v1
+git pull origin v1
 cd "../.."
 composer install
 composer dump-autoload -o

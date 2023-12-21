ffc31539c2a31a38ae146a3c2b7f0a60354efbe0
green <son5-29@mail.ru>
2023-12-22 09:03:39 +1200

    fix(update.sh)

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git fetch origin main
-git checkout main -f && git merge origin/main --no-ff -Xtheirs -m'update(auto merge with the origin/main branch)'
+git checkout main -f && git merge origin/main -Xtheirs -m'update(auto merge with the origin/main branch)'
 
 ./init.sh

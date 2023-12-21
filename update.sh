ab4ef0c8e90204ce16d9f59b2f786c1727deaf14
green <son5-29@mail.ru>
2023-12-22 08:42:39 +1200

    fix(update.sh)

--- update.sh
+++ update.sh
@@ -1,27 +1,4 @@
 git fetch origin main
-git checkout main && git merge origin/main
+git checkout main && git merge origin/main -Xtheris -m'update(auto merge with the main branch)'
 
-cd "./bundles/green-symfony"
-
-cd "./command-bundle"
-git fetch --all
-git checkout -b v1 -f -q
-git checkout v1 -f
-git merge origin/v1 --ff
-cd ".."
-
-cd "./service-bundle"
-git fetch --all
-git checkout -b v1 -f -q
-git checkout v1 -f
-git merge origin/v1 --ff
-cd ".."
-
-cd "./env-processor-bundle"
-git fetch --all
-git checkout -b v1 -f -q
-git checkout v1 -f
-git merge origin/v1 --ff
-cd ".."
-
-cd "../.."
+./init.sh

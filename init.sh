dd58dd3d65be534900dad632a3fbe37264b25534
Grin <son5-29@mail.ru>
2023-12-22 20:47:33 +1200

    fix(init.sh)

--- init.sh
+++ init.sh
@@ -7,17 +7,17 @@ git clone "https://github.com/green-symfony/env-processor-bundle.git"
 
 cd "./command-bundle"
 git checkout -b v1
-git checkout v1 && git pull origin v1
+git checkout v1 -f && git pull origin v1 -Xtheirs
 cd ".."
 
 cd "./service-bundle"
 git checkout -b v1
-git checkout v1 && git pull origin v1
+git checkout v1 -f && git pull origin v1 -Xtheirs
 cd ".."
 
 cd "./env-processor-bundle"
 git checkout -b v1
-git checkout v1 && git pull origin v1
+git checkout v1 -f && git pull origin v1 -Xtheirs
 cd ".."
 
 cd "../.."
